9ee2e0bfcec5d35c90c75d34aa1dce86c84d6a50
Diego Figueroa Guillén <A01655047@tec.mx>
2022-03-25 01:00:22 -0600

    Increased projectile bullet speed and put the current score number at the top of the middle of the game

--- Python/cannon.py
+++ Python/cannon.py
@@ -1,29 +1,28 @@
 """Cannon, hitting targets with projectiles.
-
 Exercises
-
 1. Keep score by counting target hits.
 2. Vary the effect of gravity.
 3. Apply gravity to the targets.
 4. Change the speed of the ball.
-
 """
 
 from random import randrange
 from turtle import *
 from freegames import vector
 
-ball = vector(-200, -200)
+ball = vector(-200,-200)
 speed = vector(0, 0)
 targets = []
+score = 0
+sp=5 #speed
 
 def tap(x, y):
     "Respond to screen tap."
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 25
-        speed.y = (y + 200) / 25
+        speed.x = (x*sp + 200) / 25
+        speed.y = (y*sp + 200) / 25
 
 def inside(xy):
     "Return True if xy within screen."
@@ -41,9 +40,13 @@ def draw():
         goto(ball.x, ball.y)
         dot(6, 'red')
 
+    goto(0,190)
+    write(score,font=("Arial",20))
+        
     update()
 
 def move():
+    global score
     "Move ball and targets."
     # Generate a new target at random times
     if randrange(40) == 0:
@@ -53,29 +56,33 @@ def move():
 
     # Move the existing targets
     for target in targets:
-        target.x -= 0.5
+        target.x -= 2
 
     # Move the cannon shot
     if inside(ball):
-        speed.y -= 0.35
+        speed.y -= 0.35*sp
         ball.move(speed)
 
     # Make a copy of the existing target list before redrawing
     dupe = targets.copy()
     targets.clear()
 
-    # Detect if the bullet hits a target
+   # Detect if the bullet hits a target and adds score
     for target in dupe:
         if abs(target - ball) > 13:
             targets.append(target)
+        else:
+            score += 1
+            
 
     draw()
 
     # Detect when a target reaches the left side
     for target in targets:
         if not inside(target):
+            target.x = 200
             #targets.remove(target)
-            return
+            #return
 
     ontimer(move, 50)
 
@@ -85,4 +92,5 @@ up()
 tracer(False)
 onscreenclick(tap)
 move()
-done()
+done()  
+
